3a573ec1856af724a8fefc37a73074fa2b6e65d8
Chu Oguejiofor <chuoguejiofor@gmail.com>
2018-11-01 17:27:08 -0700

    Fixed tests

--- test_calc_number_of_seconds.py
+++ test_calc_number_of_seconds.py
@@ -1,9 +1,9 @@
 import pytest
-@fixture.mark.parameterize("time_period, time_unit, expected", [(1,'day',86400), (1,'month',2592000)])
+@pytest.mark.parametrize("time_period, time_unit, expected", [(1,'day',86400), (1,'month',2592000)])
 
 def test_accuracy(time_period, time_unit, expected):
     assert calc_number_of_seconds(time_period, time_unit) == expected
 
 def test_non_time_units():
-    with pytet.raises(Exception):
+    with pytest.raises(Exception):
         calc_number_of_seconds(1, "meter")
