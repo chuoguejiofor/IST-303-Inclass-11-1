312ed221395a915bef270b9615356103b2918ed7
Chu <Chuoguejiofor@gmail.com>
2018-11-01 17:16:21 -0700

    Added exception test

--- test_calc_number_of_seconds.py
+++ test_calc_number_of_seconds.py
@@ -3,3 +3,7 @@ import pytest
 
 def test_accuracy(time_period, time_unit, expected):
     assert calc_number_of_seconds(time_period, time_unit) == expected
+
+def test_non_time_units():
+    with pytet.raises(Exception):
+        calc_number_of_seconds(1, "meter")
