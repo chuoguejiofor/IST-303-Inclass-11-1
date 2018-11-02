24093002373f941c67610cdbc384021b737d7d5e
nash90 <nash.rakuten@gmail.com>
2018-11-01 17:35:12 -0700

    added test of q2

--- in-class-2.py
+++ in-class-2.py
@@ -1,3 +1,5 @@
+import pytest
+
 KG_IN_1_POUND = 0.54
 KM_IN_1_MILE = 1.61
 LITER_IN_1_GALLON = 3.79
@@ -18,3 +20,44 @@ def convert_units(amount, current_unit, converted_unit):
 
 if __name__ == '__main__' :
     print(convert_units(1, 'Kilometer', 'Mile'))
+
+
+
+
+pound = 'Pound'
+kilogram = 'Kilogram'
+mile = 'Mile'
+kilometer = 'Kilometer'
+fahrenheit = 'Fahrenheit'
+celsius = 'Celsius'
+gallon = 'Gallon'
+liter = 'Liter'
+
+test_case = {
+(1, pound, kilogram, 0.45),
+(1, mile, kilometer, 1.61),
+(1, fahrenheit, celsius, -17.22),
+(1, gallon, liter, 3.79),
+(6, pound, kilogram, 2.72),
+(4, mile, kilometer, 6.44),
+(90, fahrenheit, celsius, 32.22),
+(8, gallon, liter, 30.28)
+}
+
+@pytest.mark.parametrize('amount,current_unit,converted_unit,expected', test_case)
+def test_convert_units(amount, current_unit, converted_unit, expected):
+    return_value = convert_units(amount, current_unit, converted_unit)
+    assert round(return_value,2) == expected
+    
+
+invalid_test_case = {
+(0.45, kilogram, pound, 1),
+(1.16, kilometer, mile, 1),
+(1, pound, 'abcdeddkdjkd', 0.45),
+}
+
+@pytest.mark.parametrize('amount,current_unit,converted_unit', invalid_test_case)
+def raise_test_convert_units(amount, current_unit, converted_unit, expected):
+    with pytest.raises(Exception):
+        convert_units(amount, current_unit, converted_unit)
+
