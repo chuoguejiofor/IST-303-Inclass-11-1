b1d5091fec646716d90b51c9cde6900422920f64
Chu Oguejiofor <chuoguejiofor@gmail.com>
2018-11-01 17:32:25 -0700

    Fixed capitalization

--- in-class-2.py
+++ in-class-2.py
@@ -2,14 +2,14 @@ KG_IN_1_POUND = 0.54
 KM_IN_1_MILE = 1.61
 LITER_IN_1_GALLON = 3.79
 
-CONVERSION_MAP = {('Fahrenheit', 'Celsius'): lambda f: 5 / 9 * (f - 32),
-                  ('Celsius', 'Fahrenheit'): lambda c: c * 9 / 5 + 32,
-                  ('Pound', 'Kilogram'): lambda p: p * KG_IN_1_POUND,
-                  ('Kilogram', 'Pound'): lambda k: k / KG_IN_1_POUND,
-                  ('Mile', 'Kilometer'): lambda k: k * KM_IN_1_MILE,
-                  ('Kilometer', 'Mile'): lambda m: m / KM_IN_1_MILE,
-                  ('Gallon', 'Liter'): lambda g: g * LITER_IN_1_GALLON,
-                  ('Liter', 'Gallon'): lambda l: l / LITER_IN_1_GALLON}
+CONVERSION_MAP = {('fahrenheit', 'celsius'): lambda f: 5 / 9 * (f - 32),
+                  ('celsius', 'fahrenheit'): lambda c: c * 9 / 5 + 32,
+                  ('pound', 'kilogram'): lambda p: p * KG_IN_1_POUND,
+                  ('kilogram', 'pound'): lambda k: k / KG_IN_1_POUND,
+                  ('mile', 'kilometer'): lambda k: k * KM_IN_1_MILE,
+                  ('kilometer', 'mile'): lambda m: m / KM_IN_1_MILE,
+                  ('gallon', 'liter'): lambda g: g * LITER_IN_1_GALLON,
+                  ('liter', 'gallon'): lambda l: l / LITER_IN_1_GALLON}
 
 
 def convert_units(amount, current_unit, converted_unit):
@@ -17,4 +17,4 @@ def convert_units(amount, current_unit, converted_unit):
 
 
 if __name__ == '__main__' :
-    print(convert_units(1, 'Kilometer', 'Mile'))
+    print(convert_units(1, 'kilometer', 'mile'))
